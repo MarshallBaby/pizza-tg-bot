efa678a6c2fdb82d218354115b87be173391b142
MarshallBaby <aleksey.yatsevich@gmail.com>
2021-02-06 16:27:40 +0300

    0.0.4

--- main.py
+++ main.py
@@ -5,6 +5,33 @@ from numpy import *
 import telebot
 import configparser
 
+
+# from pprint import pprint
+# import httplib2 
+# import apiclient.discovery
+# from oauth2client.service_account import ServiceAccountCredentials	
+
+# CREDENTIALS_FILE = 'pizza-tg-bot-be9f47d83502.json'
+# # ID Google Sheets документа (можно взять из его URL)
+# spreadsheet_id = '1ne95wg3LVvU2N-OuL1QHpyzJSxGERkabAVfOXlgQ_EQ'
+
+# # Авторизуемся и получаем service — экземпляр доступа к API
+# credentials = ServiceAccountCredentials.from_json_keyfile_name(
+#     CREDENTIALS_FILE,
+#     ['https://www.googleapis.com/auth/spreadsheets',
+#      'https://www.googleapis.com/auth/drive'])
+# httpAuth = credentials.authorize(httplib2.Http())
+# service = apiclient.discovery.build('sheets', 'v4', http = httpAuth)
+
+# values = service.spreadsheets().values().get(
+#     spreadsheetId=spreadsheet_id,
+#     range='A1:E10',
+#     majorDimension='COLUMNS'
+# ).execute()
+# pprint(values)
+
+
+
 config = configparser.ConfigParser()
 config.read("settings.ini")
 bot = telebot.TeleBot(config['Telegram']['token'])
