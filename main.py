95be38f8d84032f953eb71b489bf809ef9dac898
MarshallBaby <aleksey.yatsevich@gmail.com>
2021-02-07 22:14:26 +0300

    0.0.6

--- main.py
+++ main.py
@@ -34,18 +34,35 @@ config = configparser.ConfigParser()
 config.read("settings.ini")
 bot = telebot.TeleBot(config['Telegram']['token'])
 
-@bot.message_handler(commands=['start'])
-def user_registration(message):
-    chat_id = message.chat.id
+def is_registered(message):
     connection.commit()
-    sql = "SELECT * FROM `registered_users` WHERE `chat_id` = " + str(chat_id)
+    sql = "SELECT * FROM `registered_users` WHERE `chat_id` = " + str(message.chat.id)
     cursor.execute(sql)
     res = cursor.fetchall()
-    print(res)
     if (res == ()):
         sql = "INSERT INTO `registered_users` (`chat_id`, `first_name`, `last_name`) VALUES (%s, %s, %s)"
         cursor.execute(sql, (message.chat.id, message.from_user.first_name, message.from_user.last_name))
         connection.commit()
-        print('Done!')
-    
+
+@bot.message_handler(commands=['help'])
+def help_reaction(message):
+    bot.send_message(message.chat.id , config['Bot']['help_reply_text'])
+
+@bot.message_handler(commands=['start'])
+def user_registration(message):
+    is_registered(message)
+    bot.send_message(message.chat.id, config['Bot']['start_reply_text'])
+        
+@bot.message_handler(content_types=['text'])
+def text_reaction(message):
+    is_registered(message)
+    connection.commit()
+    sql = "SELECT `current_field` FROM `registered_users` WHERE `chat_id` =  " + str(message.chat.id)
+    cursor.execute(sql)
+    res = cursor.fetchone()[0]
+    print(res)
+    current_field = res
+    if(current_field == -1):
+        bot.send_message(message.chat.id, config['Bot']['text_no_func_reaction'])
+
 bot.polling()
