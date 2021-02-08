0f6649e971aaf92de1bee84ab59e3873777dce90
MarshallBaby <aleksey.yatsevich@gmail.com>
2021-02-08 17:15:04 +0300

    0.1.0
    Рабочий варинт

--- main-backup.py
+++ main-backup.py
@@ -43,29 +43,29 @@ connection.commit()
 #         print(result)
 
 
-# from pprint import pprint
-# import httplib2 
-# import apiclient.discovery
-# from oauth2client.service_account import ServiceAccountCredentials	
+from pprint import pprint
+import httplib2 
+import apiclient.discovery
+from oauth2client.service_account import ServiceAccountCredentials	
 
-# CREDENTIALS_FILE = 'pizza-tg-bot-be9f47d83502.json'
-# # ID Google Sheets документа (можно взять из его URL)
-# spreadsheet_id = '1ne95wg3LVvU2N-OuL1QHpyzJSxGERkabAVfOXlgQ_EQ'
+CREDENTIALS_FILE = 'pizza-tg-bot-be9f47d83502.json'
+# ID Google Sheets документа (можно взять из его URL)
+spreadsheet_id = '1ne95wg3LVvU2N-OuL1QHpyzJSxGERkabAVfOXlgQ_EQ'
 
-# # Авторизуемся и получаем service — экземпляр доступа к API
-# credentials = ServiceAccountCredentials.from_json_keyfile_name(
-#     CREDENTIALS_FILE,
-#     ['https://www.googleapis.com/auth/spreadsheets',
-#      'https://www.googleapis.com/auth/drive'])
-# httpAuth = credentials.authorize(httplib2.Http())
-# service = apiclient.discovery.build('sheets', 'v4', http = httpAuth)
+# Авторизуемся и получаем service — экземпляр доступа к API
+credentials = ServiceAccountCredentials.from_json_keyfile_name(
+    CREDENTIALS_FILE,
+    ['https://www.googleapis.com/auth/spreadsheets',
+     'https://www.googleapis.com/auth/drive'])
+httpAuth = credentials.authorize(httplib2.Http())
+service = apiclient.discovery.build('sheets', 'v4', http = httpAuth)
 
-# values = service.spreadsheets().values().get(
-#     spreadsheetId=spreadsheet_id,
-#     range='A1:E10',
-#     majorDimension='COLUMNS'
-# ).execute()
-# pprint(values)
+values = service.spreadsheets().values().get(
+    spreadsheetId=spreadsheet_id,
+    range='A1:E10',
+    majorDimension='COLUMNS'
+).execute()
+pprint(values)
 
 
 
--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from oauth2client.service_account import ServiceAccountCredentials
+import httplib2
+import apiclient.discovery
+from pprint import pprint
 import os
 import sys
 import numpy as np
@@ -13,37 +17,53 @@ config = configparser.ConfigParser()
 config.read("settings.ini")
 bot = telebot.TeleBot(config['Telegram']['token'])
 
+
+CREDENTIALS_FILE = 'pizza-tg-bot-be9f47d83502.json'
+# ID Google Sheets документа (можно взять из его URL)
+spreadsheet_id = '1dJ2D_D5UqK-UOufyqApOPam3SLOWyCJ1CxQOZUZIAHU'
+
+# Авторизуемся и получаем service — экземпляр доступа к API
+credentials = ServiceAccountCredentials.from_json_keyfile_name(
+    CREDENTIALS_FILE,
+    ['https://www.googleapis.com/auth/spreadsheets',
+     'https://www.googleapis.com/auth/drive'])
+httpAuth = credentials.authorize(httplib2.Http())
+service = apiclient.discovery.build('sheets', 'v4', http=httpAuth)
+
 # SQL подключение
 connection = pymysql.connect(
-    host = 'localhost',
-    user = 'root',
-    password = '',
-    charset = 'utf8mb4',
+    host='localhost',
+    user='root',
+    password='',
+    charset='utf8mb4',
     db='pizzatgdatabase',
     # cursorclass=DictCursor
 )
 
 cursor = connection.cursor()
 
-#SQL проверка подключения
+# SQL проверка подключения
 if connection.open != 1:
     print("SQL connection ERROR")
     sys.exit()
-else :
+else:
     print("SQL connected sucsessfully")
-    
-#TeleBot подключение
+
+# TeleBot подключение
 
 global fields_amount
 fields_amount = int(config['Table']['fields_amount'])
 
+
 def bsm(message, value):
     bot.send_message(message.chat.id, value)
+
 
 def is_registered(message):
     global fields_amount
     connection.commit()
-    sql = "SELECT * FROM `registered_users` WHERE `chat_id` = " + str(message.chat.id)
+    sql = "SELECT * FROM `registered_users` WHERE `chat_id` = " + \
+        str(message.chat.id)
     cursor.execute(sql)
     res = cursor.fetchall()
     if (res == ()):
@@ -52,102 +72,133 @@ def is_registered(message):
             data_array.append("?")
         data_array = "#".join(data_array)
         sql = "INSERT INTO `registered_users` (`chat_id`, `first_name`, `last_name`, `data_array`) VALUES (%s, %s, %s, %s)"
-        cursor.execute(sql, (message.chat.id, str(message.from_user.first_name), str(message.from_user.last_name), data_array))
+        cursor.execute(sql, (message.chat.id, str(message.from_user.first_name), str(
+            message.from_user.last_name), data_array))
         connection.commit()
         del data_array
         del sql
-        
+
+
 def current_field_get(message):
     connection.commit()
-    sql = "SELECT `current_field` FROM `registered_users` WHERE `chat_id` =  " + str(message.chat.id)
+    sql = "SELECT `current_field` FROM `registered_users` WHERE `chat_id` =  " + \
+        str(message.chat.id)
     cursor.execute(sql)
     return cursor.fetchone()[0]
 
+
 def current_field_upload(message, value):
     connection.commit()
-    sql = "UPDATE `registered_users` SET `current_field` = " + str(value) + " WHERE `chat_id` = " + str(message.chat.id)
+    sql = "UPDATE `registered_users` SET `current_field` = " + \
+        str(value) + " WHERE `chat_id` = " + str(message.chat.id)
     cursor.execute(sql)
     connection.commit()
     return 0
 
+
 def current_field_increment(message):
     connection.commit()
-    sql = "SELECT `current_field` FROM `registered_users` WHERE `chat_id` =  " + str(message.chat.id)
+    sql = "SELECT `current_field` FROM `registered_users` WHERE `chat_id` =  " + \
+        str(message.chat.id)
     cursor.execute(sql)
     current_field = cursor.fetchone()[0]
-    sql = "UPDATE `registered_users` SET `current_field` = " + str(current_field + 1) + " WHERE `chat_id` = " + str(message.chat.id)
+    sql = "UPDATE `registered_users` SET `current_field` = " + \
+        str(current_field + 1) + " WHERE `chat_id` = " + str(message.chat.id)
     cursor.execute(sql)
     connection.commit()
-    
+
+
 def data_array_get(message):
     connection.commit()
-    sql = "SELECT `data_array` FROM `registered_users` WHERE `chat_id` =  " + str(message.chat.id)
+    sql = "SELECT `data_array` FROM `registered_users` WHERE `chat_id` =  " + \
+        str(message.chat.id)
     cursor.execute(sql)
     data_array = cursor.fetchone()[0]
     return data_array.split("#")
 
+
 def data_array_upload(message, data_array):
     connection.commit()
     # data_array = "#".join(data_array)
-    sql = "UPDATE `registered_users` SET `data_array` = '" + str("#".join(data_array)) + "' WHERE `chat_id` = " + str(message.chat.id)
+    sql = "UPDATE `registered_users` SET `data_array` = '" + \
+        str("#".join(data_array)) + \
+        "' WHERE `chat_id` = " + str(message.chat.id)
     print(sql)
     cursor.execute(sql)
     connection.commit()
-    
 
-            
 
 @bot.message_handler(commands=['help'])
 def help_reaction(message):
-    bot.send_message(message.chat.id , config['Bot']['help_reply_text'])
+    bot.send_message(message.chat.id, config['Bot']['help_reply_text'])
+
 
 @bot.message_handler(commands=['start'])
 def user_registration(message):
     is_registered(message)
     bot.send_message(message.chat.id, config['Bot']['start_reply_text'])
-    
+
+
 @bot.message_handler(commands=['new'])
 def new_reaction(message):
-    is_registered(message) 
+    is_registered(message)
     if(current_field_get(message) == -1):
         current_field_increment(message)
         bsm(message, "Создание нового массива")
-        bsm(message, "Введите значение" + str(current_field_get(message)))
+        bsm(message, "Введите значение " + str(current_field_get(message) + 1))
     else:
-        print("Я еблан")
         connection.commit()
-        sql = "DELETE FROM `registered_users` WHERE `chat_id` = " + str(message.chat.id)
+        sql = "DELETE FROM `registered_users` WHERE `chat_id` = " + \
+            str(message.chat.id)
         cursor.execute(sql)
         connection.commit()
         is_registered(message)
         bsm(message, "reset")
-        
+
+
 @bot.message_handler(content_types=['text'])
 def text_reaction(message):
     global fields_amount
-    is_registered(message) 
+    is_registered(message)
     if(current_field_get(message) == -1):
-        bot.send_message(message.chat.id, config['Bot']['text_no_func_reaction'])
+        bot.send_message(
+            message.chat.id, config['Bot']['text_no_func_reaction'])
     else:
-        print(current_field_get(message))
+        data_array = data_array_get(message)
+        data_array[current_field_get(message)] = message.text
+        data_array_upload(message, data_array)
+        current_field_increment(message)
         if(current_field_get(message) >= fields_amount):
             array_send(message)
             connection.commit()
-            sql = "DELETE FROM `registered_users` WHERE `chat_id` = " + str(message.chat.id)
+            sql = "DELETE FROM `registered_users` WHERE `chat_id` = " + \
+                str(message.chat.id)
             cursor.execute(sql)
             connection.commit()
             is_registered(message)
         else:
-            data_array = data_array_get(message)
-            data_array[current_field_get(message)] = message.text
-            data_array_upload(message, data_array)
-            current_field_increment(message)
-            bsm(message, "Введите значение " + str(current_field_get(message)))
-        
-        
+            bsm(message, "Введите значение " + str(current_field_get(message) + 1))
+
+
 def array_send(message):
+    data_array = (data_array_get(message))
+    value_range_body = {
+        "values": [
+            data_array
+        ]
+    }
+    request = service.spreadsheets().values().append(
+        spreadsheetId=spreadsheet_id,
+        range='Лист1!A1:E1',
+        valueInputOption='USER_ENTERED',
+        body=value_range_body,
+
+    )
+    response = request.execute()
+    pprint(response)
+    print(data_array)
     bsm(message, "Отправляем массив")
-    print("Отправляем массив")        
+    print("Отправляем массив")
 
 
 bot.polling()
